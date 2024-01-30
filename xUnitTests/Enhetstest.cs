05b7fe8a0cb44b64ac8597d5a5bfbd56f10bc97d
NiklasAllard1 <143690578+NiklasAllard1@users.noreply.github.com>
2024-01-30 13:57:40 +0100

    First commit

---
+++ xUnitTests/Enhetstest.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using API;
+
+public class StartupTests
+{
+    private readonly Startup _startup;
+
+    public StartupTests()
+    {
+        _startup = new Startup();
+    }
+
+    [Theory]
+    [InlineData("abc", "dgf")]
+    [InlineData("ABC", "DGF")]
+    [InlineData("123", "123")]
+    [InlineData("Hej", "Kho")]
+    
+    
+    
+    public void Encrypt_GivenValidInput_ReturnsExpectedOutput(string input, string expectedOutput)
+    {
+        string result = _startup.Encrypt(input);
+        Assert.Equal(expectedOutput, result);
+    }
+
+    [Theory]
+    [InlineData("dgf", "abc")]
+    [InlineData("DGF", "ABC")]
+    [InlineData("123", "123")]
+    [InlineData("Kho", "Hej")]
+    
+    public void Decrypt_GivenValidInput_ReturnsExpectedOutput(string input, string expectedOutput)
+    {
+        string result = _startup.Decrypt(input);
+        Assert.Equal(expectedOutput, result);
+    }
+}
